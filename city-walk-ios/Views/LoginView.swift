f0819b6dc816ec7836ad910e37037906b438d442
Tyh2001 <1469442737@qq.com>
2024-08-12 17:45:11 +0800

    feat:

--- city-walk-ios/Views/LoginView.swift
+++ city-walk-ios/Views/LoginView.swift
@@ -70,16 +70,84 @@ struct LoginView: View {
                     VStack(spacing: 20) {
                         // 登录状态
                         if loginState == .login {
-                            LoginFormView(
-                                email: $email,
-                                countdownSeconds: $countdownSeconds,
-                                isCountingDown: $isCountingDown,
-                                code: $code,
-                                focus: $focus,
-                                isLoginButtonDisabled: $isLoginButtonDisabled,
-                                validateEmail: validateEmail,
-                                userLoginEmail: userLoginEmail
-                            )
+                            // 标题
+                            HStack {
+                                Image("logo")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                                    .mask(Circle())
+                                    
+                                Text("Welcome")
+                                    .font(.title)
+                                    .bold()
+                                    .foregroundStyle(.black)
+                                    
+                                Spacer()
+                            }
+                                
+                            /// 邮箱
+                            HStack {
+                                TextField("请输入邮箱", text: $email)
+                                    .padding(.vertical, 20)
+                                    .keyboardType(.default)
+                                    .focused($focus, equals: .email)
+                                    .submitLabel(.next)
+                                    .onReceive(Just(code), perform: { _ in
+                                        limitMaxLength(content: &code, maxLength: 50)
+                                    })
+                                    .onSubmit { // 监听提交事件
+                                        self.validateEmail()
+                                    }
+                                    
+                                // 获取验证码按钮
+                                if isCountingDown {
+                                    Text("\(countdownSeconds)s后再试")
+                                        .foregroundStyle(.gray)
+                                } else {
+                                    Button {
+                                        self.validateEmail()
+                                        print("获取验证码")
+                                    } label: {
+                                        Text("获取验证码")
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, 23)
+                            .background(.white, in: RoundedRectangle(cornerRadius: 35))
+                            .padding(.top, 20)
+                                
+                            /// 验证码
+                            HStack {
+                                TextField("邮箱验证码", text: $code)
+                                    .keyboardType(.numberPad) // 设置键盘类型为数字键盘
+                                    .textContentType(.oneTimeCode) // 设置内容类型为一次性代码，以便系统知道右下角按钮应该显示为“确认”
+                                    .padding(.vertical, 20)
+                                    .focused($focus, equals: .code)
+                                    .submitLabel(.return)
+                                    .onReceive(Just(code), perform: { _ in
+                                        limitMaxLength(content: &code, maxLength: 6)
+                                    }) // 最大长度为 6 位
+                            }
+                            .padding(.horizontal, 23)
+                            .background(.white, in: RoundedRectangle(cornerRadius: 35))
+                                
+                            /// 登录按钮
+                            Button {
+                                Task {
+                                    await self.userLoginEmail()
+                                }
+                                print("点击登录")
+                            } label: {
+                                Circle()
+                                    .frame(width: 80, height: 80)
+                                    .overlay {
+                                        Image(systemName: "chevron.right")
+                                            .foregroundStyle(.white)
+                                            .font(.system(size: 20))
+                                    }
+                            }
+                            .disabled(isLoginButtonDisabled)
                         }
                     
                         Spacer()
@@ -125,6 +193,16 @@ struct LoginView: View {
         let emailRegex = #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
         return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
     }
+    
+//    /// 检测输入框最大长度
+//    /// - Parameters:
+//    ///   - content: 输入框的内容
+//    ///   - maxLength: 最大长度
+//    private func limitMaxLength(content: inout String, maxLength: Int) {
+//        if content.count > maxLength {
+//            content = String(content.prefix(maxLength))
+//        }
+//    }
     
     /// 获取邮箱验证码
     private func emailSend() async {
